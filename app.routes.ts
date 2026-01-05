9d67f3d3b82e88dedf33e5308ed691f14ee3332f
Ibrahima samake <115491419+ibrahimasamake@users.noreply.github.com>
2026-01-05 21:28:07 +0530

    test

---
+++ app.routes.ts
@@ -0,0 +1,31 @@
+import { Routes } from '@angular/router';
+import {HomeComponent} from './features/home/home/home.component';
+
+export const routes: Routes = [
+
+  {path: '',pathMatch:"full", component:HomeComponent},
+  {path: 'home',pathMatch:"full", redirectTo: ""},
+  {
+    path: 'about',
+    pathMatch:"full",
+    loadComponent: () =>
+      import('./features/about/about-house-info/about-house-info.component')
+        .then(m => m.AboutHouseInfoComponent),
+  },
+  {
+    path: 'reservation',
+    pathMatch:"full",
+    loadComponent: () =>
+      import('./features/about/about-house-info/about-house-info.component')
+        .then(m => m.AboutHouseInfoComponent),
+  },
+  {
+    path: 'contact',
+    pathMatch:"full",
+    loadComponent: () =>
+      import('./features/about/about-house-info/about-house-info.component')
+        .then(m => m.AboutHouseInfoComponent),
+  },
+
+
+];
